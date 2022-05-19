48a685311f5f5efd5375b3dc0b1baaae11221c2f
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-05-19 23:11:44 +0600

    data show on UI

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const port = 3001;
+
+app.use(cors());
+app.use(bodyParser.json());
 
 const { MongoClient } = require("mongodb");
 const uri =
@@ -9,14 +15,33 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-client.connect((err) => {
-  const collection = client.db("burjAlArab").collection("bookings");
-  console.log("Database Connected Successfully");
-  client.close();
-});
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+client.connect((err) => {
+  const bookings = client.db("burjAlArab").collection("bookings");
+  console.log("Database connection Successfully");
+
+  //Create
+  app.post("/addBooking", (req, res) => {
+    const newBooking = req.body;
+    bookings.insertOne(newBooking).then((res) => {
+      res.send(res.insertedCount > 0);
+    });
+    console.log(newBooking);
+  });
+
+  //Get API of Own Database - Read
+  app.get("/bookings", (req, res) => {
+    // console.log(req.query.email);
+
+    bookings.find({ email: req.query.email }).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
 });
+//?email=", +loggedInUser.email email: req.query.email
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.listen(port);
